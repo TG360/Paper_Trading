08fdd04ef9da6cc46a9b5a396be4093076d458a0
TG360 <rrmereddy@tamu.edu>
2023-09-18 00:27:06 -0500

    done

--- app.py
+++ app.py
@@ -3,34 +3,17 @@ import os
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from tempfile import mkdtemp
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
-
 
 from helpers import apology, login_required, lookup, usd
 
 # Configure application
 app = Flask(__name__)
 
-# Ensure templates are auto-reloaded
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-
-
-# Ensure responses aren't cached
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
-
-
 # Custom filter
 app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
@@ -38,24 +21,15 @@ Session(app)
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///finance.db")
 
-# Make sure API key is set
-if not os.environ.get("API_KEY"):
-    raise RuntimeError("API_KEY not set")
 
-@app.route("/add_cash", methods=["GET", "POST"])
-@login_required
-def add_cash():
-    if request.method == "POST":
-        db.execute("""
-        UPDATE users
-        SET cash = cash + :amount
-        WHERE id=:user_id
-        """, amount = request.form.get("cash"),
-        user_id=session["user_id"])
-        flash("Added Cash!")
-        return redirect("/")
-    else:
-        return render_template("add.html")
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 
 @app.route("/")
 @login_required
@@ -86,7 +60,22 @@ def index():
     
     return render_template("index.html", holdings=holdings, cash=usd(cash), grand_total=usd(grand_total))
 
-
+@app.route("/add_cash", methods=["GET", "POST"])
+@login_required
+def add_cash():
+    if request.method == "POST":
+        db.execute("""
+        UPDATE users
+        SET cash = cash + :amount
+        WHERE id=:user_id
+        """, amount = request.form.get("cash"),
+        user_id=session["user_id"])
+        flash("Added Cash!")
+        return redirect("/")
+    else:
+        return render_template("add.html")
+    
+    
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
 def buy():
@@ -142,7 +131,6 @@ def history():
     for i in range(len(transactions)):
         transactions[i]["price"] = usd(transactions[i]["price"])
     return render_template("history.html", transactions=transactions)
-    return apology("TODO")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -213,7 +201,7 @@ def quote():
         return render_template("quote.html")
 
 
-@app.route("/register", methods=["GET","POST"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
 
@@ -304,16 +292,3 @@ def sell():
         HAVING SUM(shares) > 0;
     """, user_id = session["user_id"])
         return render_template("sell.html", symbols=[ row["symbol"] for row in rows])
-
-
-
-def errorhandler(e):
-    """Handle error"""
-    if not isinstance(e, HTTPException):
-        e = InternalServerError()
-    return apology(e.name, e.code)
-
-
-# Listen for errors
-for code in default_exceptions:
-    app.errorhandler(code)(errorhandler)
